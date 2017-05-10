dcf869494eb9efdf5383a006b836171b690d422d
je3f0o <je3f0o@gmail.com>
2017-05-10 17:05:44 +0800

    Better caching for multiple jeefo instance

--- src/core.js
+++ src/core.js
@@ -37,78 +37,131 @@ snake_case = function (str) {
 	});
 },
 
-to_string          = Object.prototype.toString,
-function_to_string = Function.toString,
+_to_string          = Object.prototype.toString,
+_function_to_string = Function.toString,
 
-IS_DIGITS_SIGNED_INT      = /^\-?\d+$/,
-IS_DIGITS_UNSIGNED_INT    = /^\d+$/,
-IS_DIGITS_SIGNED_NUMBER   = /^\-?\d+(?:.\d+)?$/,
-IS_DIGITS_UNSIGNED_NUMNER = /^\d+(?:.\d+)?$/,
+is_date = {
+	fn : function () {
+		var to_string = _to_string;
 
-// Used to detect host constructors (Safari > 4; really typed array specific)
-HOST_CONSTRUCTOR_REGEX = /^\[object .+?Constructor\]$/,
-/*
-// Compile a regexp using a common native method as a template.
-// We chose `Object#toString` because there's a good chance it is not being mucked with.
-new RegExp('^' +
-	// Coerce `Object#toString` to a string
-	String(to_string).
-		// Escape any special regexp characters
-		replace(/[.*+?^${}()|[\]\/\\]/g, "\\$&").
-		// Replace mentions of `toString` with `.*?` to keep the template generic.
-		// Replace thing like `for ...` to support environments like Rhino which add extra info
-		// such as method arity.
-		replace(/toString|(function).*?(?=\\\()| for .+?(?=\\\])/g, "$1.*?") + '$'
-)
-*/
-NATIVE_REGEX = /^function.*?\(\) \{ \[native code\] \}$/,
-
-is_date = function (value) {
-	return to_string.call(value) === "[object Date]";
+		return function (value) {
+			return to_string.call(value) === "[object Date]";
+		};
+	}
 },
 
-is_regex = function (value) {
-	return to_string.call(value) === "[object RegExp]";
+is_regex = {
+	fn : function () {
+		var to_string = _to_string;
+
+		return function (value) {
+			return to_string.call(value) === "[object RegExp]";
+		};
+	}
 },
 
-is_digits = function (value, is_unsigned) {
-	return (is_unsigned ? IS_DIGITS_UNSIGNED_NUMNER : IS_DIGITS_SIGNED_NUMBER).test(value);
+is_digits = {
+	IS_DIGITS_SIGNED_NUMBER   : /^\-?\d+(?:.\d+)?$/,
+	IS_DIGITS_UNSIGNED_NUMNER : /^\d+(?:.\d+)?$/,
+
+	fn : function () {
+		var IS_DIGITS_SIGNED_NUMBER   = this.IS_DIGITS_SIGNED_NUMBER,
+			IS_DIGITS_UNSIGNED_NUMNER = this.IS_DIGITS_UNSIGNED_NUMNER;
+
+		return function (value, is_unsigned) {
+			return (is_unsigned ? IS_DIGITS_UNSIGNED_NUMNER : IS_DIGITS_SIGNED_NUMBER).test(value);
+		};
+	}
 },
 
-is_digits_int = function (value, is_unsigned) {
-	return (is_unsigned ? IS_DIGITS_UNSIGNED_INT : IS_DIGITS_SIGNED_INT).test(value);
+is_digits_int = {
+	IS_DIGITS_SIGNED_INT   : /^\-?\d+$/,
+	IS_DIGITS_UNSIGNED_INT : /^\d+$/,
+
+	fn : function () {
+		var IS_DIGITS_SIGNED_INT   = this.IS_DIGITS_SIGNED_INT,
+			IS_DIGITS_UNSIGNED_INT = this.IS_DIGITS_UNSIGNED_INT;
+
+		return function (value, is_unsigned) {
+			return (is_unsigned ? IS_DIGITS_UNSIGNED_INT : IS_DIGITS_SIGNED_INT).test(value);
+		};
+	}
 },
 
-is_native = function (value) {
-	var type = typeof value;
-	return type === "function" ?
-		// Use `Function#toString` to bypass the value's own `toString` method
-		// and avoid being faked out.
-		NATIVE_REGEX.test(function_to_string.call(value)) :
-		// Fallback to a host object check because some environments will represent
-		// things like typed arrays as DOM methods which may not conform to the
-		// normal native pattern.
-		(value && type === "object" && HOST_CONSTRUCTOR_REGEX.test(to_string.call(value))) || false;	
+is_native = {
+	// Used to detect host constructors (Safari > 4; really typed array specific)
+	HOST_CONSTRUCTOR_REGEX : /^\[object .+?Constructor\]$/,
+	/*
+	// Compile a regexp using a common native method as a template.
+	// We chose `Object#toString` because there's a good chance it is not being mucked with.
+	new RegExp('^' +
+		// Coerce `Object#toString` to a string
+		String(to_string).
+			// Escape any special regexp characters
+			replace(/[.*+?^${}()|[\]\/\\]/g, "\\$&").
+			// Replace mentions of `toString` with `.*?` to keep the template generic.
+			// Replace thing like `for ...` to support environments like Rhino which add extra info
+			// such as method arity.
+			replace(/toString|(function).*?(?=\\\()| for .+?(?=\\\])/g, "$1.*?") + '$'
+	)
+	*/
+	NATIVE_REGEX : /^function.*?\(\) \{ \[native code\] \}$/,
+
+	fn : function () {
+		var NATIVE_REGEX           = this.NATIVE_REGEX,
+			HOST_CONSTRUCTOR_REGEX = this. HOST_CONSTRUCTOR_REGEX,
+			to_string              = _to_string,
+			function_to_string     = _function_to_string;
+
+		return function (value) {
+			var type = typeof value;
+			return type === "function" ?
+				// Use `Function#toString` to bypass the value's own `toString` method
+				// and avoid being faked out.
+				NATIVE_REGEX.test(function_to_string.call(value)) :
+				// Fallback to a host object check because some environments will represent
+				// things like typed arrays as DOM methods which may not conform to the
+				// normal native pattern.
+				(value && type === "object" && HOST_CONSTRUCTOR_REGEX.test(to_string.call(value))) || false;	
+		};
+	}
 },
 
-json_parse = function (value) {
-	try {
-		return JSON.parse(value);
-	} catch (e) {}
+json_parse = {
+	JSON : JSON,
+	fn : function () {
+		return function (value) {
+			try {
+				return this.JSON.parse(value);
+			} catch (e) {}
+		};
+	}
 },
 
-JeefoObject = function () {};
+createJeefoObject = function (assign) {
+	var JeefoObject = function () {};
 
-JeefoObject.prototype = {
-	Array       : Array,
-	JeefoObject : JeefoObject,
-	$new : function () {
-		return new this.JeefoObject();
-	},
-	$copy : function () {
-		return this.assign(new this.JeefoObject(), this);
-	},
-};
+	JeefoObject.create = function (object) {
+		return assign(new JeefoObject(), object);
+	};
+
+	JeefoObject.prototype = {
+		Array       : Array,
+		assign      : assign,
+		JeefoObject : JeefoObject,
+		$new : function () {
+			return new this.JeefoObject();
+		},
+		$copy : function () {
+			return this.assign(new this.JeefoObject(), this);
+		},
+	};
+
+	return {
+		fn : function () { return JeefoObject; }
+	};
+},
+jeefo_object;
 
 core_module.extend("namespace", ["$injector", "make_injectable"], function (injector, make_injectable) {
 	return function (full_name) {
@@ -145,20 +198,6 @@ core_module.extend("namespace", ["$injector", "make_injectable"], function (inje
 
 		return this;
 	};
-}).
-
-namespace("JeefoObject", ["object.assign"], function (assign) {
-	if (JeefoObject.create) {
-		JeefoObject.create = function (object) {
-			return assign(new JeefoObject(), object);
-		};
-	}
-
-	if (! JeefoObject.prototype.assign) {
-		JeefoObject.prototype.assign = assign;
-	}
-
-	return JeefoObject;
 }).
 
 namespace("transform.dash_case", function () {
@@ -232,27 +271,19 @@ extend("service", [
 	};
 }).
 
-run("$injector", function ($injector) {
+run(["$injector", "object.assign"], function ($injector, assign) {
+	if (! jeefo_object) {
+		jeefo_object = createJeefoObject(assign);
+	}
 
-	$injector.register("is_date", {
-		fn : function () { return is_date; }
-	}).
-	register("is_regex", {
-		fn : function () { return is_regex; }
-	}).
-	register("is_digit", {
-		fn : function () { return is_digits; }
-	}).
-	register("is_digit_int", {
-		fn : function () { return is_digits_int; }
-	}).
-	register("is_native", {
-		fn : function () { return is_native; }
-	}).
-	register("json_parse", {
-		fn : function () { return json_parse; }
-	});
-
+	$injector.
+		register("is_date"      , is_date).
+		register("is_regex"     , is_regex).
+		register("is_digit"     , is_digits).
+		register("is_digit_int" , is_digits_int).
+		register("is_native"    , is_native).
+		register("json_parse"   , json_parse).
+		register("JeefoObject"  , jeefo_object);
 });
 
 // ignore:start
