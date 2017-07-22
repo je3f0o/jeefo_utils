69d7a1e50df2e905a6675afc996bd3729b787154
je3f0o <je3f0o@gmail.com>
2017-07-22 10:18:36 +0800

    Bug fixed

--- src/core.js
+++ src/core.js
@@ -1,29 +1,21 @@
 /* -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.
 * File Name   : core.js
 * Created at  : 2017-04-08
-* Updated at  : 2017-07-01
+* Updated at  : 2017-07-22
 * Author      : jeefo
 * Purpose     :
 * Description :
 _._._._._._._._._._._._._._._._._._._._._.*/
 //ignore:start
-"use strict";
 
-/* globals -IS_FUNCTION, -IS_STRING */
+/* globals */
 /* exported */
-
-var IS_FUNCTION = function (x) {
-	return typeof x === "function";
-};
-var IS_STRING = function (x) {
-	return typeof x === "string";
-};
 
 module.exports = function (jeefo) {
 
 //ignore:end
 
-var core_module = jeefo.module("jeefo_core", []),
+var core_module = jeefo.module("jeefo.core", []),
 
 CAMEL_CASE_REGEXP = /[A-Z]/g,
 dash_case = function (str) {
@@ -35,133 +27,7 @@ snake_case = function (str) {
 	return str.replace(CAMEL_CASE_REGEXP, function (letter, pos) {
 		return (pos ? '_' : '') + letter.toLowerCase();
 	});
-},
-
-_to_string          = Object.prototype.toString,
-_function_to_string = Function.toString,
-
-is_date = {
-	fn : function () {
-		var to_string = _to_string;
-
-		return function (value) {
-			return to_string.call(value) === "[object Date]";
-		};
-	}
-},
-
-is_regex = {
-	fn : function () {
-		var to_string = _to_string;
-
-		return function (value) {
-			return to_string.call(value) === "[object RegExp]";
-		};
-	}
-},
-
-is_digits = {
-	IS_DIGITS_SIGNED_NUMBER   : /^\-?\d+(?:.\d+)?$/,
-	IS_DIGITS_UNSIGNED_NUMNER : /^\d+(?:.\d+)?$/,
-
-	fn : function () {
-		var IS_DIGITS_SIGNED_NUMBER   = this.IS_DIGITS_SIGNED_NUMBER,
-			IS_DIGITS_UNSIGNED_NUMNER = this.IS_DIGITS_UNSIGNED_NUMNER;
-
-		return function (value, is_unsigned) {
-			return (is_unsigned ? IS_DIGITS_UNSIGNED_NUMNER : IS_DIGITS_SIGNED_NUMBER).test(value);
-		};
-	}
-},
-
-is_digits_int = {
-	IS_DIGITS_SIGNED_INT   : /^\-?\d+$/,
-	IS_DIGITS_UNSIGNED_INT : /^\d+$/,
-
-	fn : function () {
-		var IS_DIGITS_SIGNED_INT   = this.IS_DIGITS_SIGNED_INT,
-			IS_DIGITS_UNSIGNED_INT = this.IS_DIGITS_UNSIGNED_INT;
-
-		return function (value, is_unsigned) {
-			return (is_unsigned ? IS_DIGITS_UNSIGNED_INT : IS_DIGITS_SIGNED_INT).test(value);
-		};
-	}
-},
-
-is_native = {
-	// Used to detect host constructors (Safari > 4; really typed array specific)
-	HOST_CONSTRUCTOR_REGEX : /^\[object .+?Constructor\]$/,
-	/*
-	// Compile a regexp using a common native method as a template.
-	// We chose `Object#toString` because there's a good chance it is not being mucked with.
-	new RegExp('^' +
-		// Coerce `Object#toString` to a string
-		String(to_string).
-			// Escape any special regexp characters
-			replace(/[.*+?^${}()|[\]\/\\]/g, "\\$&").
-			// Replace mentions of `toString` with `.*?` to keep the template generic.
-			// Replace thing like `for ...` to support environments like Rhino which add extra info
-			// such as method arity.
-			replace(/toString|(function).*?(?=\\\()| for .+?(?=\\\])/g, "$1.*?") + '$'
-	)
-	*/
-	NATIVE_REGEX : /^function.*?\(\) \{ \[native code\] \}$/,
-
-	fn : function () {
-		var NATIVE_REGEX           = this.NATIVE_REGEX,
-			HOST_CONSTRUCTOR_REGEX = this. HOST_CONSTRUCTOR_REGEX,
-			to_string              = _to_string,
-			function_to_string     = _function_to_string;
-
-		return function (value) {
-			var type = typeof value;
-			return type === "function" ?
-				// Use `Function#toString` to bypass the value's own `toString` method
-				// and avoid being faked out.
-				NATIVE_REGEX.test(function_to_string.call(value)) :
-				// Fallback to a host object check because some environments will represent
-				// things like typed arrays as DOM methods which may not conform to the
-				// normal native pattern.
-				(value && type === "object" && HOST_CONSTRUCTOR_REGEX.test(to_string.call(value))) || false;	
-		};
-	}
-},
-
-json_parse = {
-	JSON : JSON,
-	fn : function () {
-		return function (value) {
-			try {
-				return this.JSON.parse(value);
-			} catch (e) {}
-		};
-	}
-},
-
-createJeefoObject = function (assign) {
-	var JeefoObject = function () {};
-
-	JeefoObject.create = function (object) {
-		return assign(new JeefoObject(), object);
-	};
-
-	JeefoObject.prototype = {
-		Array       : Array,
-		assign      : assign,
-		JeefoObject : JeefoObject,
-		$new : function () {
-			return new this.JeefoObject();
-		},
-		$copy : function () {
-			return this.assign(new this.JeefoObject(), this);
-		},
-	};
-
-	return {
-		fn : function () { return JeefoObject; }
-	};
-},
-jeefo_object;
+};
 
 core_module.extend("namespace", ["$injector", "make_injectable"], function (injector, make_injectable) {
 	return function (full_name) {
@@ -211,48 +77,34 @@ namespace("transform.snake_case", function () {
 extend("curry", [
 	"$injector",
 	"make_injectable",
-	"transform.snake_case",
-], function ($injector, make_injectable, snake_case) {
+], function ($injector, make_injectable) {
 	return function (name) {
-		$injector.register(snake_case(`${ name }Curry`), make_injectable.apply(null, arguments));
+		$injector.register(`${ name }_curry`, make_injectable.apply(null, arguments));
 		return this;
 	};
 }).
 
 extend("run", ["$injector", "$q", "Array"], function ($injector, $q, Arr) {
-	var instance = this;
+	var self = this;
 
 	return function (dependencies, fn) {
-		if (IS_FUNCTION(dependencies)) {
-			dependencies.call(this);
-		} else if (IS_STRING(dependencies)) {
-			$injector.resolve(dependencies).then(function (value) {
-				fn.call(instance, value);
-			});
-		} else {
-			var	args = new Arr(dependencies.length);
-
-			$q.for_each_async(dependencies, function (dependency, index, next) {
-				$injector.resolve(dependency).then(function (value) {
-					args[index] = value;
-					next();
-				});
-			}).then(function () {
-				fn.apply(instance, args);
-			});
+		var i = dependencies.length, resolvers = new Arr(i);
+		while (i--) {
+			resolvers[i] = $injector.resolve(dependencies[i]);
 		}
 
-		return this;
+		return $q.all(resolvers).then(function (values) {
+			return fn.apply(self, values);
+		});
 	};
 }).
 
 extend("factory", [
 	"$injector",
 	"make_injectable",
-	"transform.snake_case",
-], function ($injector, make_injectable, snake_case) {
+], function ($injector, make_injectable) {
 	return function (name) {
-		$injector.register(snake_case(`${ name }Factory`), make_injectable.apply(null, arguments));
+		$injector.register(`${ name }_factory`, make_injectable.apply(null, arguments));
 		return this;
 	};
 }).
@@ -271,18 +123,104 @@ extend("service", [
 }).
 
 run(["$injector", "object.assign"], function ($injector, assign) {
-	if (! jeefo_object) {
-		jeefo_object = createJeefoObject(assign);
-	}
+	var to_string                 = Object.prototype.toString,
+		JeefoObject               = function () {},
+		function_to_string        = Function.toString,
+		IS_DIGITS_SIGNED_INT      = /^\-?\d+$/,
+		IS_DIGITS_UNSIGNED_INT    = /^\d+$/,
+		IS_DIGITS_SIGNED_NUMBER   = /^\-?\d+(?:.\d+)?$/,
+		IS_DIGITS_UNSIGNED_NUMNER = /^\d+(?:.\d+)?$/,
+
+		// Used to detect host constructors (Safari > 4; really typed array specific)
+		HOST_CONSTRUCTOR_REGEX = /^\[object .+?Constructor\]$/,
+		/*
+		// Compile a regexp using a common native method as a template.
+		// We chose `Object#toString` because there's a good chance it is not being mucked with.
+		new RegExp('^' +
+			// Coerce `Object#toString` to a string
+			String(to_string).
+				// Escape any special regexp characters
+				replace(/[.*+?^${}()|[\]\/\\]/g, "\\$&").
+				// Replace mentions of `toString` with `.*?` to keep the template generic.
+				// Replace thing like `for ...` to support environments like Rhino which add extra info
+				// such as method arity.
+				replace(/toString|(function).*?(?=\\\()| for .+?(?=\\\])/g, "$1.*?") + '$'
+		)
+		*/
+		NATIVE_REGEX = /^function.*?\(\) \{ \[native code\] \}$/,
+
+		is_date = function (value) {
+			return to_string.call(value) === "[object Date]";
+		},
+
+		is_regex = function (value) {
+			return to_string.call(value) === "[object RegExp]";
+		},
+
+		is_digit = function (value, is_unsigned) {
+			return (is_unsigned ? IS_DIGITS_UNSIGNED_NUMNER : IS_DIGITS_SIGNED_NUMBER).test(value);
+		},
+
+		is_digit_int = function (value, is_unsigned) {
+			return (is_unsigned ? IS_DIGITS_UNSIGNED_INT : IS_DIGITS_SIGNED_INT).test(value);
+		},
+
+		is_native = function (value) {
+			var type = typeof value;
+			return type === "function" ?
+				// Use `Function#toString` to bypass the value's own `toString` method
+				// and avoid being faked out.
+				NATIVE_REGEX.test(function_to_string.call(value)) :
+				// Fallback to a host object check because some environments will represent
+				// things like typed arrays as DOM methods which may not conform to the
+				// normal native pattern.
+				(value && type === "object" && HOST_CONSTRUCTOR_REGEX.test(to_string.call(value))) || false;	
+		},
+
+		json_parse = function (value) {
+			try {
+				return JSON.parse(value);
+			} catch (e) {}
+		};
+
+	JeefoObject.create = function (object) {
+		return assign(new JeefoObject(), object);
+	};
+
+	JeefoObject.prototype = {
+		Array       : Array,
+		assign      : assign,
+		JeefoObject : JeefoObject,
+		$new : function () {
+			return new this.JeefoObject();
+		},
+		$copy : function () {
+			return this.assign(new this.JeefoObject(), this);
+		},
+	};
 
 	$injector.
-		register("is_date"      , is_date).
-		register("is_regex"     , is_regex).
-		register("is_digit"     , is_digits).
-		register("is_digit_int" , is_digits_int).
-		register("is_native"    , is_native).
-		register("json_parse"   , json_parse).
-		register("JeefoObject"  , jeefo_object);
+		register("is_date", {
+			fn : function () { return is_date; }
+		}).
+		register("is_regex", {
+			fn : function () { return is_regex; }
+		}).
+		register("is_digit", {
+			fn : function () { return is_digit; }
+		}).
+		register("is_digit_int", {
+			fn : function () { return is_digit_int; }
+		}).
+		register("is_native", {
+			fn : function () { return is_native; }
+		}).
+		register("json_parse", {
+			fn : function () { return json_parse; }
+		}).
+		register("JeefoObject", {
+			fn : function () { return JeefoObject; }
+		});
 });
 
 // ignore:start
@@ -291,5 +229,34 @@ run(["$injector", "object.assign"], function ($injector, assign) {
 var jeefo = require("jeefo").create();
 
 jeefo.use(module.exports);
+
+var c = jeefo.module("test", ["jeefo.core"]);
+c.run([
+	"is_date",
+], function (is_date) {
+	console.log("is_date", is_date(new Date()));
+});
+
+c = jeefo.module("test2", ["test"]);
+c = jeefo.module("test3", ["test2"]);
+c = jeefo.module("test4", ["test3"]);
+
+c.run([
+	"is_date",
+	"is_regex",
+	"is_digit",
+	"is_digit_int",
+	"is_native",
+	"json_parse",
+	"JeefoObject",
+], function (is_date, is_regex, is_digit, is_digit_int, is_native, json_parse, JeefoObject) {
+	console.log("is_date", is_date(new Date()));
+	console.log("is_regex", is_regex(/asd/));
+	console.log("is_digit", is_digit("23.33"));
+	console.log("is_digit_int", is_digit_int("23"));
+	console.log("is_native", is_native(setTimeout));
+	console.log("json_parse", json_parse('{ "s" : 2 }'));
+	console.log("JeefoObject", new JeefoObject());
+});
 
 // ignore:end
