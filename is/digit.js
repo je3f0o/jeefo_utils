411c4ff3ba9e5a8e842a620358277159f295c6b4
je3f0o <je3f0o@gmail.com>
2017-09-01 08:17:40 +0800

    Snake case added

--- is/digit.js
+++ is/digit.js
@@ -1,7 +1,7 @@
 /* -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.
 * File Name   : digit.js
 * Created at  : 2017-08-09
-* Updated at  : 2017-08-09
+* Updated at  : 2017-08-24
 * Author      : jeefo
 * Purpose     :
 * Description :
@@ -13,6 +13,9 @@ _._._._._._._._._._._._._._._._._._._._._.*/
 
 // ignore:end
 
-module.exports = function () {
-	
+var IS_DIGITS_SIGNED_NUMBER   = /^\-?\d+(?:.\d+)?$/,
+	IS_DIGITS_UNSIGNED_NUMNER = /^\d+(?:.\d+)?$/;
+
+module.exports = function (value, is_unsigned) {
+	return (is_unsigned ? IS_DIGITS_UNSIGNED_NUMNER : IS_DIGITS_SIGNED_NUMBER).test(value);
 };
---
+++ string/snake_case.js
@@ -0,0 +1,22 @@
+/* -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.
+* File Name   : snake_case.js
+* Created at  : 2017-09-01
+* Updated at  : 2017-09-01
+* Author      : jeefo
+* Purpose     :
+* Description :
+_._._._._._._._._._._._._._._._._._._._._.*/
+// ignore:start
+
+/* globals */
+/* exported */
+
+// ignore:end
+
+var CAMEL_CASE_REGEXP = /[A-Z]/g;
+
+module.exports = function (str) {
+	return str.replace(CAMEL_CASE_REGEXP, function (letter, pos) {
+		return (pos ? '_' : '') + letter.toLowerCase();
+	});
+};
