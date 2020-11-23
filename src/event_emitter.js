9697f70832cfab2fef68cb6d04cb3f7a7812a9e1
je3f0o <je3f0o@gmail.com>
2020-11-23 19:11:45 +0800

    v0.0.11

--- src/event_emitter.js
+++ src/event_emitter.js
@@ -1,7 +1,7 @@
 /* -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.
 * File Name   : event_emitter.js
 * Created at  : 2019-09-17
-* Updated at  : 2020-10-22
+* Updated at  : 2020-11-23
 * Author      : jeefo
 * Purpose     :
 * Description :
@@ -23,41 +23,34 @@ class EventEmitter {
     constructor (is_private = false) {
         if (! is_private) {
             this._events = {};
-            return;
+            return this;
         }
         const events = Object.create(null);
 
         const readonly = new Readonly(this);
         readonly.prop("on", (event_name, listener) => {
-            if (! events[event_name]) {
-                events[event_name] = [];
-            }
+            if (! events[event_name]) events[event_name] = [];
             events[event_name].push(listener);
             return listener;
         }, false);
 
         const off_handler = (event_name, listener) => {
-            if (events[event_name]) {
-                array_remove(events[event_name], listener);
-            }
+            if (events[event_name]) array_remove(events[event_name], listener);
         };
         readonly.prop("off", off_handler, false);
         readonly.prop("removeListener", off_handler, false);
 
         readonly.prop("emit", function (event_name, ...args) {
             if (events[event_name]) {
-                const listeners = events[event_name].concat();
-                listeners.forEach(listener => {
+                for (const listener of events[event_name].concat()) {
                     listener.apply(this, args);
-                });
+                }
             }
         }, false);
     }
 
     on (event_name, listener) {
-        if (! this._events[event_name]) {
-            this._events[event_name] = [];
-        }
+        if (! this._events[event_name]) this._events[event_name] = [];
         this._events[event_name].push(listener);
         return listener;
     }
@@ -65,18 +58,15 @@ class EventEmitter {
     removeListener (event_name, listener) {
         if (this._events[event_name]) {
             const index = this._events[event_name].indexOf(listener);
-            if (index >= 0) {
-                this._events[event_name].splice(index, 1);
-            }
+            if (index >= 0) this._events[event_name].splice(index, 1);
         }
     }
 
     emit (event_name, ...args) {
         if (this._events[event_name]) {
-            const listeners = this._events[event_name].concat();
-            listeners.forEach(listener => {
+            for (const listener of this._events[event_name].concat()) {
                 listener.apply(this, args);
-            });
+            }
         }
     }
 
